e8bdbf500f3d1956e0457ef587d828c00fef3f07
Lechu <you@example.com>
2021-11-23 17:57:23 +0530

    Known website

---
+++ script.js
@@ -0,0 +1,8 @@
+function sum() {
+    let num1 = parseInt(prompt("Enter the first number"));
+    let num2 = parseInt(prompt("Enter the second number"));
+    let sum1 = num1+num2 ;
+    return sum1;
+}
+alert("The sum is " +sum());
+
